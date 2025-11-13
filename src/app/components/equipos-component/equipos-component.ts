e6a8ea228c97a3cbb26cee5653cfee770ebcb541
Kevbast <kevbast936@gmail.com>
2025-11-13 13:36:31 +0100

    menu + equipos

--- src/app/components/equipos-component/equipos-component.ts
+++ src/app/components/equipos-component/equipos-component.ts
@@ -3,12 +3,14 @@ import { Equipo } from '../../../models/equipo';
 import { ServiceFutbol } from '../../../services/service.futbol';
 import { ActivatedRoute, Params } from '@angular/router';
 import { DatosEquipo } from '../../../models/datosequipo';
+import { forkJoin } from 'rxjs';
+import { delay, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-equipos-component',
   standalone: false,
   templateUrl: './equipos-component.html',
-  styleUrl: './equipos-component.css',
+  styleUrls: ['./equipos-component.css'],
 })
 export class EquiposComponent implements OnInit{
 
@@ -18,24 +20,20 @@ public idEquiporecibido!:number;
 
 constructor(private _service:ServiceFutbol,private _activeRoute:ActivatedRoute){}
 ngOnInit(): void {
-  this._activeRoute.params.subscribe((parametros:Params)=>{
-    let idEquipo=parseInt(parametros['idEquipo']);
-
-    let datos:DatosEquipo;
-    datos= new DatosEquipo();
-
-    //procedemos con los servicios:
-    this._service.findEquipo(idEquipo).subscribe(response=>{
-      datos.equipo=response;
+  this._activeRoute.params.pipe(
+    switchMap((parametros: Params) => {
+      const idEquipo = parseInt(parametros['idEquipo']);
+      return forkJoin({
+        equipo: this._service.findEquipo(idEquipo),
+        jugadores: this._service.getJugadoresEquipo(idEquipo).pipe(delay(2000))//retrasamos un poco una petición
+      });
     })
-
-    this._service.getJugadoresEquipo(idEquipo).subscribe(response=>{
-      datos.jugadores=response;
-    })
-
-    
-
-  })
+  ).subscribe(result => {
+    const datos = new DatosEquipo();
+    datos.equipo = result.equipo;
+    datos.jugadores = result.jugadores;
+    this.datosEquipo = datos;
+  });
 
 }
 
